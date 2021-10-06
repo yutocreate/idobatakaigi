2bfdc22686b2f248ec58db956d643436c72d6264
idenyuto <1otuy26@gmail.com>
2021-10-06 15:49:23 +0900

    create setName

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -42,7 +42,7 @@ const useStyles = makeStyles((theme) => ({
     margin: theme.spacing(3, 0, 2),
   },
 }));
-export default function SignIn() {
+export default function SignIn({setName}) {
   const classes = useStyles();
   return (
     <Container component="main" maxWidth="xs">
